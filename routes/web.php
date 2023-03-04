55ff39548b21855b1ad1220909a5e688365f277d
Jaqueline <84473714+Jaqueedm@users.noreply.github.com>
2023-03-04 00:03:51 -0600

    Update web.php
    prueba 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,51 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('sucursales', function (){
+    return "vista sucursal";
+});
+
+Route::get('sucursales/create', function (){
+    return "vista para crear una sucursal";
+});
+
+Route::get('sucursales/{sucursal}', function ($sucursal){
+    return "vista sucursal: $sucursal";
+});
+
+Route::get('sucursales/{sucursal}/edit', function ($sucursal){
+    return "vista editar";
+});
+
+Route::put('sucursales/{sucursal}', function ($sucursal){
+    return "vista acatualizar";
+});
+
+Route::delete('sucursales/{sucursal}', function ($sucursal){
+    return "vista eliminar";
+});
+
+Route::get('servidores', function (){
+    return "vista sucursal";
+});
+
+Route::get('servidores/create', function (){
+    return "vista para crear una sucursal";
+});
+
+Route::get('servidores/{servidor}', function ($servidor){
+    return "vista sucursal: $sucursal";
+});
+
+Route::get('servidores/{servidor}/edit', function ($servidor){
+    return "vista editar";
+});
+
+Route::put('servidores/{servidores}', function ($servidor){
+    return "vista acatualizar";
+});
+
+Route::delete('servidores/{servidores}', function ($servidor){
+    return "vista eliminar";
+});
